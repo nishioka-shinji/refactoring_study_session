1b1661c8ca13208d97f2e4dc476146af4f8bb65e
nishioka-shinji <>
2021-08-21 01:46:34 +0900

    ポイント計算処理を価格クラスへ

--- lib/childrens_price.rb
+++ lib/childrens_price.rb
@@ -1,4 +1,7 @@
 class ChildrensPrice
+  require 'default_price'
+  include DefaultPrice
+
   def charge(days_rented)
     result = 1.5
     result += (days_rented - 3) * 1.5 if days_rented > 3
--- lib/regular_price.rb
+++ lib/regular_price.rb
@@ -1,4 +1,7 @@
 class RegularPrice
+  require 'default_price'
+  include DefaultPrice
+
   def charge(days_rented)
     result = 2
     result += (days_rented - 2) * 1.5 if days_rented > 2
